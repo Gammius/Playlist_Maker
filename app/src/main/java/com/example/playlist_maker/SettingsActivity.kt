133692b62eb5fc49110777af8d90e9f0ba41e218
Gammius <misha_baltika@mail.ru>
2024-07-26 11:45:35 +0300

    home work 2

--- app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
@@ -1,10 +1,8 @@
 package com.example.playlist_maker
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.playlist_maker.R
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +11,7 @@ class SettingsActivity : AppCompatActivity() {
 
         val buttonBackSettings = findViewById<Button>(R.id.arrow_back)
         buttonBackSettings.setOnClickListener{
-            val intentBackSettings = Intent(this, MainActivity::class.java)
-            startActivity(intentBackSettings)
+            onBackPressed()
         }
     }
 }
